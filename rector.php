f3450c5f3c6527140fbce1685d3c7bfa3f510176
Gabriel do Carmo Vieira <48625433+gvieira18@users.noreply.github.com>
2025-10-13 09:29:27 -0300

    chore(deps): update lint-staged, filament packages, and rector versions

--- rector.php
+++ rector.php
@@ -26,11 +26,10 @@ use RectorLaravel\Set\LaravelSetList;
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/app',
-        __DIR__.'/config',
         __DIR__.'/bootstrap/app.php',
+        __DIR__.'/config',
         __DIR__.'/database',
         __DIR__.'/routes',
-        __DIR__.'/public',
         __DIR__.'/tests',
     ])
     ->withCache(
